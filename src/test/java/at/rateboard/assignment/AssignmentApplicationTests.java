16d70575640bea191bb455d08229adbbf57ceb8f
Bernhard Wittmann <dev@bernhardwittmann.com>
2020-11-02 16:16:27 +0100

    feat: Import Spring Boilerplate

---
+++ src/test/java/at/rateboard/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package at.rateboard.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
